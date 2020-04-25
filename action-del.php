3a428ff3e340a1e7e6496e21d9b1cfd8c2ad9f57
ZhangXiang7 <47569530+ZhangXiang7@users.noreply.github.com>
2020-04-25 16:45:21 -0400

    Update action-del.php

--- action-del.php
+++ action-del.php
@@ -2,7 +2,7 @@
 require('db.php');
 
 $id = $_GET['id'];
-$query = "DELETE FROM `case` WHERE CASE_NUMBER={$id}";
+$query = "DELETE FROM `case` WHERE CASE_NUMBER='{$id}'";
 					
 
 mysqli_query($con,$query) or die(mysql_error());
